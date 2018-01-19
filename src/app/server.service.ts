813d3905b7384bf58478034a8b6831b7979af746
Ivona Malekovic <ivonamalekovic@hotmail.com>
2018-01-19 10:50:46 +0100

    http with firebase, get, post, put, map

---
+++ src/app/server.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from "@angular/core";
+import { Http, Headers, Response } from "@angular/http";
+import { Observable } from "rxjs/Observable";
+import "rxjs/Rx";
+
+@Injectable()
+export class ServerService {
+    constructor(private http: Http) {  }
+    
+    
+    storeServers(servers: any []) : Observable<any> {
+        const headers = new Headers({"Content-Type": "application/json"});
+     //   return this.http.post("https://udemy-ng-http-ed289.firebaseio.com/data.json", servers, {headers: headers});
+        return this.http.put("https://udemy-ng-http-ed289.firebaseio.com/data.json", servers, {headers: headers});
+    }
+
+    getServers(){
+        return this.http.get("https://udemy-ng-http-ed289.firebaseio.com/data.json")
+            .map(
+                (response: Response) => {
+                    const data = response.json();
+                    for (const server of data){
+                        server.name = "FETCHED_" + server.name;
+                    }
+                    return data;
+                }
+            )
+    }
+}
